0402fd25690248db390d48e01c15c5be19bda70f
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-03 15:05:08 +0100

    feat: modifiche

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,15 +3,15 @@ export default function Card({ attrici }) {
   return (
     <div className="col">
       <div className="card h-100">
-        <img src={image} className="card-img-top " alt={name} />
+        <img src={image} className="card-img-top h-50" alt={name} />
 
         <div className="card-body">
-          <h5 className="card-title">{name}</h5>
-          <p className="card-text">
+          <h5 className="card-title fs-2">{name}</h5>
+          <p className="card-text fs-5">
             {birth_year} {nationality}
           </p>
-          <p className="card-text">{biography}</p>
-          <p>{awards}</p>
+          <p className="card-text fs-5">{biography}</p>
+          <p className="card-text fs-5">{awards}</p>
         </div>
       </div>
     </div>
