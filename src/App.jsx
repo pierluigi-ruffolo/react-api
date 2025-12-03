cd73d1c83d34acca2f730d0fcbb707c55419b8ac
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-03 14:10:19 +0100

    milestone 1

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,29 @@
-import { useState } from "react";
-
+import { useEffect, useState } from "react";
+import axios from "axios";
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-function App() {}
+function App() {
+  const [rispostaAPi, setRispostaAPi] = useState([]);
+
+  useEffect(() => {
+    fetchAPi();
+  }, []);
+
+  function fetchAPi() {
+    axios
+      .get("https://lanciweb.github.io/demo/api/actresses/")
+      .then((risposta) => {
+        console.log(risposta.data);
+        setRispostaAPi(risposta.data);
+      });
+  }
+
+  return (
+    <div className="container">
+      <h1 className="text-center mt-5">ACTRESSES</h1>
+    </div>
+  );
+}
 
 export default App;
