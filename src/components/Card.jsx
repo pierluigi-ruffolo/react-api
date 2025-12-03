cd24af8015f00c38809200d13c7c5c7d34f0fe39
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-03 14:59:25 +0100

    feat: aggiungo immagine

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,8 @@ export default function Card({ attrici }) {
   return (
     <div className="col">
       <div className="card h-100">
-        {/*  <img src={image} className="card-img-top" alt="...">  */}
+        <img src={image} className="card-img-top " alt={name} />
+
         <div className="card-body">
           <h5 className="card-title">{name}</h5>
           <p className="card-text">
