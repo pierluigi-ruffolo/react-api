4d9a84a901ab8ab0924b2b818a3ac562e7eb1645
pierluigi-ruffolo <pierluigiruffolo1@gmail.com>
2025-12-03 16:20:23 +0100

    feat: bonus 2

--- src/App.jsx
+++ src/App.jsx
@@ -6,14 +6,21 @@ import Card from "./components/Card";
 
 function App() {
   const [rispostaAPiAttrici, setRispostaAPiAttrici] = useState([]);
-
   const [rispostaAPiAttori, setRispostaApiAttori] = useState([]);
 
+  const [rispostaAttrici, setrispostaAttrici] = useState(false);
+  const [rispostaAttori, setrispostaAttori] = useState(false);
   const [attoriAttrici, setAttoriAttrici] = useState([]);
 
   useEffect(() => {
     fetchApi();
   }, []);
+
+  useEffect(() => {
+    if (rispostaAttrici === true && rispostaAttori === true) {
+      setAttoriAttrici([...rispostaAPiAttrici, ...rispostaAPiAttori]);
+    }
+  }, [rispostaAttrici, rispostaAttori, rispostaAPiAttrici, rispostaAPiAttori]);
 
   function fetchApi() {
     axios
@@ -21,12 +28,14 @@ function App() {
       .then((risposta) => {
         setRispostaAPiAttrici(risposta.data);
         console.log(risposta.data);
+        setrispostaAttrici(true);
       });
 
     axios
       .get("https://lanciweb.github.io/demo/api/actors/")
       .then((risposta) => {
         setRispostaApiAttori(risposta.data);
+        setrispostaAttori(true);
       });
   }
 
@@ -41,6 +50,10 @@ function App() {
         {rispostaAPiAttori.map((attori) => (
           <Card key={attori.id} attrici={attori}></Card>
         ))}
+        {attoriAttrici.map((a, index) => (
+          <Card key={index} attrici={a}></Card>
+        ))}
+
         {/*  fine colonna con card  */}
       </div>
     </div>
